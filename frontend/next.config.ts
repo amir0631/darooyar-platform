941a7f20e288f269f33315537a7fdb6573787359
Sina Hardani <amir00631@gmail.com>
2025-10-18 10:56:07 +0330

    Delete frontend directory

--- frontend/next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // basePath: '/darooyar', // This is now disabled for local development
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
--- frontend/src/app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-// frontend/src/app/dashboard/layout.tsx
-// ورژن ۱.۰
-// اعمال ساختار داشبورد به تمام صفحات فرزند
-
-import DashboardLayout from "@/components/DashboardLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
