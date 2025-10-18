96d34994f4d3b77516b788389e51dbd762724f51
Sina Hardani <amir00631@gmail.com>
2025-10-18 10:55:50 +0330

    Delete backend directory

--- backend/inquiries/serializers.py
+++
@@ -1,9 +0,0 @@
-# backend/inquiries/serializers.py
-from rest_framework import serializers
-from .models import DrugInquiry
-
-class DrugInquirySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DrugInquiry
-        fields = '__all__'
-        read_only_fields = ('requester',)
